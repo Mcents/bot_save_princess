5d108b6f3e95b100c5dd18dd2ebdccc3106a6462
Michael Centrelli <acentrelli@gmail.com>
2018-02-23 11:35:19 -0500

    Pass Test for Princess and Mario Locations

--- save_princess/lib/princess.rb
+++ save_princess/lib/princess.rb
@@ -1,8 +1,20 @@
 class Princess
 
-  attr_reader :grid
+  attr_reader :grid, :p_location, :m_location
 
   def initialize(size, grid)
     @grid = grid
+    @p_location = []
+    @m_location = []
+  end
+
+  def princess_and_mario_location
+    grid.each_with_index do |row, index|
+      if row.include?("p")
+        p_location.push(index, row.chars.index("p"))
+      elsif row.include?("m")
+        m_location.push(index, row.chars.index("m"))
+      end
+    end
   end
 end
--- save_princess/spec/princess_spec.rb
+++ save_princess/spec/princess_spec.rb
@@ -13,8 +13,9 @@ describe "Princess" do
 
   it "can initialize with locations for Princess and Mario" do
     princess = Princess.new(3,["-p-", "m--", "---"])
+    princess.princess_and_mario_location
 
-    expect(princess.p_location).to eq([0,2])
-    expect(princess.m_location).to eq([1,1])
+    expect(princess.p_location).to eq([0,1])
+    expect(princess.m_location).to eq([1,0])
   end
 end
