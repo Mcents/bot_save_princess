ddb0c4e74079dfb1eff3a2094c83117f030b6416
Michael Centrelli <acentrelli@gmail.com>
2018-02-23 14:16:47 -0500

    Add Test for Initialize Princess with Grid

--- save_princess_two/spec/princess_spec.rb
+++ save_princess_two/spec/princess_spec.rb
@@ -2,5 +2,9 @@ require 'rspec'
 require_relative '../lib/princess'
 
 describe "Princess" do
+  it "initializes with grid" do
+    princess = Princess.new(3,1,2,["---", "-m-", "--p"])
 
+    expect(princess.grid).to eq(["---", "-m-", "--p"])
+  end
 end
